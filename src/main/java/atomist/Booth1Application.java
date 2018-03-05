1185c5b8a48b883340c69b5d58df2e778f1108b9
Rod Johnson <johnsonroda@gmail.com>
2018-03-05 12:34:18 +0000

    Initial commit from Atomist

---
+++ src/main/java/atomist/Booth1Application.java
@@ -0,0 +1,12 @@
+package atomist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Booth1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Booth1Application.class, args);
+	}
+}
